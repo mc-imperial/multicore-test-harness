6020e082c1bdf5d5e42b2759972319cca6a08201
diorga <iorgadan90@yahoo.com>
2018-07-14 15:15:59 +0100

    Added vector addition SUT

---
+++ src/vector_add/vector_add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+static size_t MM = 90000000;
+
+void add(double * restrict a, double * restrict b, double * restrict c, size_t n){
+    size_t i;
+    for(i = 0; i < n; ++i) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+int main() {
+    double * restrict a, * restrict b, * restrict c;
+    size_t i;
+    clock_t start,end;
+    double diffs;
+
+    a = (double *) malloc(MM*sizeof(double));
+    b = (double *) malloc(MM*sizeof(double));
+    c = (double *) malloc(MM*sizeof(double));
+
+
+    for(i = 0; i < MM; ++i){
+        a[i] = 1.0/(double)(i+1);
+        b[i] = a[i];
+    }
+
+    start = clock();
+    add(a,b,c,MM);
+    end = clock();
+    diffs = (end - start)/(double)CLOCKS_PER_SEC;
+
+    printf("time = %lf\n",diffs);
+
+    free(a);
+    free(b);
+    free(c);
+    return 0;
+}
