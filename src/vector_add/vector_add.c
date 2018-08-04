90fd4f6997af33b0b508327df43ae3fb4c8bd283
diorga <iorgadan90@yahoo.com>
2018-08-04 19:41:32 +0100

    Smaller memory

--- src/vector_add/vector_add.c
+++ src/vector_add/vector_add.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-static size_t MM = 900000;
+static size_t MM = 32000;
 
-void add(double * restrict a, double * restrict b, double * restrict c, size_t n){
+void add(float * restrict a, float * restrict b, float * restrict c, size_t n){
     size_t i;
     for(i = 0; i < n; ++i) {
         c[i] = a[i] + b[i];
@@ -12,25 +12,25 @@ void add(double * restrict a, double * restrict b, double * restrict c, size_t n
 }
 
 int main() {
-    double * restrict a, * restrict b, * restrict c;
+    float * restrict a, * restrict b, * restrict c;
     size_t i;
     clock_t start,end;
-    double diffs;
+    float diffs;
 
-    a = (double *) malloc(MM*sizeof(double));
-    b = (double *) malloc(MM*sizeof(double));
-    c = (double *) malloc(MM*sizeof(double));
+    a = (float *) malloc(MM*sizeof(float));
+    b = (float *) malloc(MM*sizeof(float));
+    c = (float *) malloc(MM*sizeof(float));
 
 
     for(i = 0; i < MM; ++i){
-        a[i] = 1.0/(double)(i+1);
+        a[i] = 1.0/(float)(i+1);
         b[i] = a[i];
     }
 
     start = clock();
     add(a,b,c,MM);
     end = clock();
-    diffs = (end - start)/(double)CLOCKS_PER_SEC;
+    diffs = (end - start)/(float)CLOCKS_PER_SEC;
 
     printf("time(ns)=%lf\n",diffs*1000000);
 
