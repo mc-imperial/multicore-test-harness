208c90bdb91ea2977f813677acc2b2661166bcb8
diorga <iorgadan90@yahoo.com>
2018-09-07 15:58:43 +0100

    Adapted the calculate rank function to the new and pretty version way of doing things. Reorganised all the JSON file folders.

--- scripts/calculate_rank.py
+++ scripts/calculate_rank.py
@@ -1,49 +1,75 @@
-import itertools
-from pprint import pprint
+import sys
 import json
 
-# Read the configuration in the JSON file
-with open("rank_joule.json") as data_file:
-    experiments_object = json.load(data_file)
 
-STRESSES = ['../tuned_enemies/tuned_cache', '../tuned_enemies/tuned_bus', '../tuned_enemies/tuned_memory']
-CORES = experiments_object["cache_rank"]["cores"]
-# print(STRESSES)
-# print(CORES)
+class CalculateRank(object):
+    def __init__(self, input_file):
+        self._input_file = input_file
 
-ALL_COMBOS = [p for p in itertools.product(STRESSES, repeat=CORES)]
+    def get_rank(self):
+        # Read the configuration in the JSON file
+        with open(self._input_file) as data_file:
+            experiments_object = json.load(data_file)
 
-conf_mapping = dict()
+        # Sort all the configurations in a list
+        dict_list = list()
+        for experiment in experiments_object:
+            ranked_list = experiments_object[experiment]["ranked_list"]
+            od = list(sorted(ranked_list.values(), key=lambda x:x['score'], reverse=True))
+            dict_list.append(od)
+
+        # For each environment. get the rank in the other experiments and store in 'rank'
+        for it in dict_list[0]:
+            environment = it['env']
+            rank_list = list()
+            # Look it up for each victim(experiment)
+            for it2 in dict_list:
+                # Find its rank there
+                for i in range(len(it2)):
+                    env = it2[i]['env']
+                    if environment == env:
+                        rank_here = i
+                        break
+                rank_list.append(rank_here)
+            it['rank'] = rank_list
+
+        # Identify the ones that are not Pareto optimal
+        rank_list_bad = list()
+        for it1 in dict_list[0]:
+            for it2 in dict_list[0]:
+                if len([i for i, j in zip(it1['rank'], it2['rank']) if i > j]) == len(it1):
+                    rank_list_bad.append(it1)
+
+        # Put the Pareto Optimal in a list
+        paretto_optimal = list()
+        for it in dict_list[0]:
+            if not (it in rank_list_bad):
+                paretto_optimal.append(it)
+
+        # If there are ties, try to break them at fewer comparisons
+        if len(paretto_optimal) > 1:
+            rank_list_bad = list()
+            for it1 in paretto_optimal:
+                for it2 in paretto_optimal:
+                    if len([i for i, j in zip(it1['rank'], it2['rank']) if i > j]) == len(it1) - 1:
+                        rank_list_bad.append(it1)
+
+            # Put the tie broken ones in a list
+            paretto_optimal_tie_break = list()
+            for it in paretto_optimal:
+                if not (it in rank_list_bad):
+                    paretto_optimal_tie_break.append(it)
+
+            print(paretto_optimal_tie_break)
+        else:
+            print(paretto_optimal)
 
 
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("usage: " + sys.argv[0] + " <ranked_environments>.json\n")
+        exit(1)
 
-result = []
+    rank = CalculateRank(sys.argv[1])
 
-for check in ALL_COMBOS:
-    found_counter = False
-    # print(check)
-
-    str = "{1: '" + check[0] + "', 2: '" + check[1] + "', 3: '" + check[2] +"'}"
-
-    bus_rank0 = [x[0] for x in experiments_object["bus_rank"]["ranked_list"]].index(str)
-    cache_rank0 = [x[0] for x in experiments_object["cache_rank"]["ranked_list"]].index(str)
-    memory_rank0 = [x[0] for x in experiments_object["mem_rank"]["ranked_list"]].index(str)
-    # print(str)
-    # print(bus_rank0, cache_rank0, memory_rank0)
-
-    for check_prime in ALL_COMBOS:
-        str2 = "{1: '" + check_prime[0] + "', 2: '" + check_prime[1] + "', 3: '" + check_prime[2] + "'}"
-        bus_rank1 = [x[0] for x in experiments_object["bus_rank"]["ranked_list"]].index(str2)
-        cache_rank1 = [x[0] for x in experiments_object["cache_rank"]["ranked_list"]].index(str2)
-        memory_rank1 = [x[0] for x in experiments_object["mem_rank"]["ranked_list"]].index(str2)
-
-        if bus_rank1 > bus_rank0 and cache_rank1 > cache_rank0 and memory_rank1 > memory_rank0:
-            found_counter = True
-            break
-
-    if found_counter:
-        continue
-
-    result.append((check, cache_rank0, memory_rank0, bus_rank0))
-
-pprint(result)
+    rank.get_rank()
