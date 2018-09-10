dba8af3b84e1497406159b7bc09d582a45285a94
diorga <iorgadan90@yahoo.com>
2018-09-10 17:24:52 +0100

    Made the overlap script more generic and avoided hardcoding

--- scripts/confidence_overlap.py
+++ scripts/confidence_overlap.py
@@ -24,86 +24,98 @@ from scipy.stats.mstats import mquantiles
 from scipy.stats import binom
 import sys
 import json
+from pprint import pprint
+import itertools
+
+QUANTILE = .9
+CONF_INTERVAL = .9
 
 
-def confidence_interval(x, quantile, desired_confidence=.9):
+class Confidence:
+    def __init__(self, argv):
 
-    assert isinstance(x, list)
-    x.sort()
-    q = mquantiles(x, quantile)[0]
-    n = len(x)
+        self._input_data = list()
+        for i in range(1, len(argv)):
+            with open(argv[i]) as data_file:
+                self._input_data.append(json.load(data_file))
 
-    confidence = 0
+    def _confidence_interval(self, x, quantile, desired_confidence=CONF_INTERVAL):
 
-    middle = round(quantile * n)
-    ui = middle
-    li = middle
-    while confidence < desired_confidence:
-        if ui < n-1:
-            ui = ui + 1
-        if li > 0:
-            li = li-1
-        confidence = binom.cdf(ui-1, n, quantile) - binom.cdf(li-1, n, quantile)
+        assert isinstance(x, list)
+        x.sort()
+        q = mquantiles(x, quantile)[0]
+        n = len(x)
 
-        if ui >= n-1 and li <= 0:
-            li = 0
-            ui = n-1
-            break
+        confidence = 0
 
-    lower_range = x[li]
-    upper_range = x[ui]
+        middle = round(quantile * n)
+        ui = middle
+        li = middle
+        while confidence < desired_confidence:
+            if ui < n-1:
+                ui = ui + 1
+            if li > 0:
+                li = li-1
+            confidence = binom.cdf(ui-1, n, quantile) - binom.cdf(li-1, n, quantile)
 
-    return lower_range, upper_range
+            if ui >= n-1 and li <= 0:
+                li = 0
+                ui = n-1
+                break
+
+        lower_range = x[li]
+        upper_range = x[ui]
+
+        return lower_range, upper_range
+
+    @staticmethod
+    def _get_overlap(a, b):
+        return a[1]>b[0] and b[1]>a[0]
+
+    def run(self):
+
+        comparison = dict()
+        for experiment in self._input_data[0]:
+            comparison[experiment] = dict()
+
+        for experiment in self._input_data[0]:
+            time_enemy = dict()
+            for i, data in enumerate(self._input_data):
+                # I do not really care whee you the baseline from
+                time_baseline = data[experiment]["time_list_baseline"]
+                # Time enemy
+                time_enemy[data[experiment]['stress']] = data[experiment]["time_list_enemy"]
 
 
-def getOverlap(a, b):
-    return a[1]>b[0] and b[1]>a[0]
+            baseline_q = mquantiles(time_baseline, .9)[0]
 
-with open("final_joule3.json") as data_file:
-    auto_object = json.load(data_file)
+            for comb in itertools.combinations(time_enemy, 2):
 
-with open("assembly_stress_joule_small.json") as data_file:
-    small_object = json.load(data_file)
+                confidence_interval1 = self._confidence_interval(time_enemy[comb[0]], QUANTILE)
+                confidence_interval2 = self._confidence_interval(time_enemy[comb[1]], QUANTILE)
 
-with open("assembly_stress_joule_larger.json") as data_file:
-    large_object = json.load(data_file)
+                confidence_overlap = self._get_overlap(confidence_interval1, confidence_interval2)
 
-comparison = dict()
-for experiment in auto_object:
-    comparison[experiment] = dict()
+                first_q = mquantiles(time_enemy[comb[0]], QUANTILE)[0]
+                second_q = mquantiles(time_enemy[comb[1]], QUANTILE)[0]
 
-for experiment in auto_object:
-    time_baseline = auto_object[experiment]["time_list_baseline"]
-    time_auto = auto_object[experiment]["time_list_enemy"]
-    time_small = small_object[experiment]["time_list_enemy"]
-    time_large = large_object[experiment]["time_list_enemy"]
+                comparison[experiment]["baseline_quantile"] = baseline_q
+                comparison[experiment][comb[0] + "_quantile"] = first_q / baseline_q
+                comparison[experiment][comb[1] + "_quantile"] = second_q / baseline_q
 
-    baseline = confidence_interval(time_baseline, .9)
-    auto = confidence_interval(time_auto,.9)
-    small = confidence_interval(time_small, .9)
-    large= confidence_interval(time_large, .9)
+                comparison[experiment][comb[0] + "_range"] = [x / baseline_q for x in confidence_interval1]
+                comparison[experiment][comb[1] + "_range"] = [x / baseline_q for x in confidence_interval2]
 
-    overlap_auto_small = getOverlap(auto, small)
-    overlap_auto_large = getOverlap(auto, large)
-    overlap_small_large = getOverlap(small, large)
+                comparison[experiment][comb[0] + "\n" + comb[1] + "_overlap"] = confidence_overlap
 
-    baseline_q = mquantiles(time_baseline, .9)[0]
-    auto_q = mquantiles(time_auto, .9)[0]
-    small_q = mquantiles(time_small, .9)[0]
-    large_q = mquantiles(time_large, .9)[0]
+        with open("overlaps.json", 'w') as outfile:
+            json.dump(comparison, outfile, sort_keys=True, indent=4)
 
-    comparison[experiment]["baseline_quantile"] = baseline_q
-    comparison[experiment]["auto_quantile"] = auto_q / baseline_q
-    comparison[experiment]["small_quantile"] = small_q / baseline_q
-    comparison[experiment]["large_quantile"] = large_q / baseline_q
 
-    comparison[experiment]["auto"] = [x/baseline_q for x in auto]
-    comparison[experiment]["small"] = [x/baseline_q for x in small]
-    comparison[experiment]["large"] = [x/baseline_q for x in large]
-    comparison[experiment]["overlap_auto_small"] = overlap_auto_small
-    comparison[experiment]["overlap_auto_large"] = overlap_auto_large
-    comparison[experiment]["overlap_small_large"] = overlap_small_large
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("usage: " + sys.argv[0] + " <list_data1>.json <list_data2>.json\n")
+        exit(1)
 
-with open("overlaps.json", 'w') as outfile:
-    json.dump(comparison, outfile, sort_keys=True, indent=4)
-
+    rank = Confidence(sys.argv)
+    rank.run()
