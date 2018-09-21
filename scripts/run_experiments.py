66f739c69d4373ffb3910af65cfd7870a2ca15d8
diorga <iorgadan90@yahoo.com>
2018-09-21 11:23:55 +0100

    Decreased number of iterations further

--- scripts/run_experiments.py
+++ scripts/run_experiments.py
@@ -330,7 +330,7 @@ class Experiment(object):
                 (time_list_enemy, temp_list_enemy) = s.run_mapping(sut=self._sut[0],
                                                                    mapping=self._mapping,
                                                                    iterations_step=self._iterations,
-                                                                   iterations_max=10000,
+                                                                   iterations_max=1000,
                                                                    fixed_iteration=True,
                                                                    max_temperature=self._max_temperature)
                 output[experiment] = self._log_data2(self._sut,
